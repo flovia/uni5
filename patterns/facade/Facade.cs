a437facec09c980193408f9eb4c1b1c67028f2f7
keycattie <cedjhjd.loeen@gmail.com>
2020-02-17 20:05:01 +0300

    facade task done

---
+++ patterns/facade/Facade.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace facade
+{
+    class Pump
+    {
+        public readonly string pumpSID;
+        private float refuelSpeed; // liters per second
+        public bool vacant = true;
+
+        public void Refuel(Cistern cistern, float ammount)
+        {
+            var fuelAmmount = cistern.Refuel(ammount);
+            if ((fuelAmmount == 0) || (!vacant))
+            {
+                Console.WriteLine($"WARNING. Cannot refuel at pump {pumpSID}, {ammount} liters of \"{cistern.fuelType}\".");
+                return;
+            }
+            vacant = false;
+            Console.WriteLine($"Refueling at pump {pumpSID}, {ammount} liters of \"{cistern.fuelType}\".");
+            System.Threading.Tasks.Task.Delay(Convert.ToInt32(ammount * 1000 / refuelSpeed)).ContinueWith(_ =>
+            {
+                Console.WriteLine($"Refueling at pump {pumpSID} finished.");
+                vacant = true;
+            });
+            return; // redundant
+        }
+
+        public Pump(string sid, float rs)
+        {
+            pumpSID = sid;
+            refuelSpeed = rs;
+        }
+    }
+
+    class Cistern
+    {
+        public readonly string fuelType;
+        public float fuelLeft;
+
+        public float Refuel(float amount)
+        {
+            if (fuelLeft - amount <= 0)
+                return 0.0f;
+            fuelLeft -= amount;
+            return fuelLeft;
+        }
+
+        public Cistern(string type, float cap)
+        {
+            fuelType = type;
+            fuelLeft = cap;
+        }
+    }
+
+    class Station
+    {
+        private List<Cistern> cisterns;
+        private List<Pump> pumps;
+
+        public void Refuel(string sid, string type, float ammount)
+        {
+            foreach (var c in cisterns)
+            {
+                if (type.Equals(c.fuelType))
+                {
+                    foreach (var p in pumps)
+                    {
+                        if (sid.Equals(p.pumpSID))
+                        {
+                            p.Refuel(c, ammount);
+                            break;
+                        }
+                    }
+                    return;
+                }
+            }
+            // on error
+            Console.WriteLine($"WARNING. Unknown refueling error, pump \"{sid}\", {ammount} liters of \"{type}\".");
+        }
+
+        public string Report()
+        {
+            string s = "            **** REPORT ****\n";
+            foreach (var c in cisterns)
+                s += $"\tCistern for \"{c.fuelType}\", {c.fuelLeft} liters left.\n";
+            foreach (var p in pumps)
+                s += $"\tPump \"{p.pumpSID}\", vacant: {p.vacant}\n";
+            return s;
+        }
+
+        public Station(List<KeyValuePair<string, float>> c,
+                       List<KeyValuePair<string, float>> p)
+        {
+            cisterns = new List<Cistern>();
+            pumps = new List<Pump>();
+            foreach (var val in c) cisterns.Add(new Cistern(val.Key, val.Value));
+            foreach (var val in p) pumps.Add(new Pump(val.Key, val.Value));
+        }
+    }
+}
---
+++ patterns/facade/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace facade
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cisterns = new List<KeyValuePair<string, float>>();
+            cisterns.Add(new KeyValuePair<string, float>("DT", 174.78f));
+            cisterns.Add(new KeyValuePair<string, float>("95", 130.04f));
+            cisterns.Add(new KeyValuePair<string, float>("98", 0.0f));
+            var pumps = new List<KeyValuePair<string, float>>();
+            pumps.Add(new KeyValuePair<string, float>("Main 1", 14.0f));
+            pumps.Add(new KeyValuePair<string, float>("Main 2", 12.6f));
+            pumps.Add(new KeyValuePair<string, float>("Reserve", 9.3f));
+
+            var st = new Station(cisterns, pumps);
+
+            st.Refuel("Main 1", "DT", 90f);
+            st.Refuel("Main 2", "95", 12.5f);
+            await Task.Delay(500);
+            st.Refuel("Reserve", "DT", 8.3f);
+            await Task.Delay(2500);
+            st.Refuel("Main 1", "95", 38f);
+            st.Refuel("Main 2", "DT", 73f);
+            st.Refuel("Main 2", "98", 5f);
+            await Task.Delay(3500);
+            st.Refuel("Main", "DT", 35f);
+            st.Refuel("Main 1", "95", 13f);
+            await Task.Delay(1000);
+            st.Refuel("Main 2", "98", 5f);
+            st.Refuel("Reserve", "95", 13f);
+            st.Refuel("Main 1", "95", 10f);
+            await Task.Delay(5000);
+            st.Refuel("Main", "DT", 35f);
+            st.Refuel("Main 1", "95", 13f);
+            await Task.Delay(1500);
+            st.Refuel("Main 2", "98", 5f);
+            st.Refuel("Main 2", "DT", 56f);
+
+            await Task.Delay(3000);
+            st.Refuel("Main 2", "95", 8f);
+            Console.WriteLine(st.Report());
+            await Task.Delay(1500);
+        }
+    }
+}
